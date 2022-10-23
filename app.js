ee2bb6445147f1335de1d67092b05ebee8492f55
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-23 10:35:56 -0300

    button style

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const Meca = require('./models/Meca')
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
+const hora = Date.now();
 
 mongoose.connect('mongodb://localhost:27017/meca', {
     useNewUrlParser: true,
@@ -23,14 +24,14 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/meca', (req, res) => {
-    res.render('home')
+app.get('/meca', async (req, res) => {
+    const mecas = await Meca.find({});
+    res.render('home', { mecas })
 })
 
 app.post('/meca', async (req, res) => {
     const { speed, mistakes } = req.body.meca
     const meca = new Meca(req.body.meca);
-    const hora = Date.now();
     const indice = speed / (mistakes*10)
     meca.date = hora;
     meca.indice = indice;
